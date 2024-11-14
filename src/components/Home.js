87a55a6627b605dbbd69427d8c3877c49a0b62fe
Perpetual Akinyi <you@example.com>
2024-11-15 01:57:48 +0300

    added static data to my components

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <h1>Welcome to Portfolio Builder</h1>
+    
+        <p>
+            This is a simple portfolio builder application. You can add, edit and delete projects from your portfolio
+        </p>
+    </div>
+  )
+}
+
+export default Home
