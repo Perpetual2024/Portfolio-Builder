46e23656c43c3bc25a1f40f0586c96080323f87c
Perpetual Akinyi <you@example.com>
2024-11-15 01:42:42 +0300

    added a route router to my app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import NavBar from './components/NavBar';
 
 function App() {
-  return 
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/skills" component={Skills} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
+
