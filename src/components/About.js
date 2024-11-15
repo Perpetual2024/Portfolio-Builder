d1ea9e5adcdef0dff623c52aa95f93cc1657a9ba
Perpetual Akinyi <you@example.com>
2024-11-15 03:01:38 +0300

    fetched data for the about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function About() {
+  const [aboutData, setAboutData] = useState({});
+
+  useEffect(() => {
+    fetch('http://localhost:3000/about')
+      .then(response => response.json())
+      .then(data => setAboutData(data));
+  }, []);
+
   return (
-    <div>About</div>
-  )
+    <div>
+      <h1>About Me</h1>
+      <img src={aboutData.profileImage} alt="Profile" />
+      <p>Name: {aboutData.name}</p>
+      <p>Bio: {aboutData.bio}</p>
+    </div>
+  );
 }
 
-export default About
+export default About;
