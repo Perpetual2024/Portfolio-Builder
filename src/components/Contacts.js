f579f6fdb4119f3a1e737d1e191b3ddaacee3b75
Perpetual Akinyi <you@example.com>
2024-11-15 03:06:32 +0300

    used the usestate hooks and useeffect hook to fetch data and return a json

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-function Contacts() {
+function Contact() {
+  const [contact, setContact] = useState({});
+
+  useEffect(() => {
+    fetch('http://localhost:3000/contact')
+      .then(response => response.json())
+      .then(data => setContact(data));
+  }, []);
+
   return (
-    <div>Contacts</div>
-  )
+    <div>
+      <h1>Contact Me</h1>
+      <p>Email: {contact.email}</p>
+      <p>Phone: {contact.phone}</p>
+      <p>Address: {contact.address}</p>
+    </div>
+  );
 }
 
-export default Contacts
+export default Contact;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,6 @@
-import React from 'react'
-
-function Projects() {
-  return (
-    <div>Projects</div>
-  )
-}
-
-export default Projects
+import React, { useState, useEffect } from 'react'; 
+function Projects() { const [projects, setProjects] = useState([]); 
+  useEffect(() => { fetch('http://localhost:3000/projects') 
+    .then(response => response.json()) 
+    .then(data => setProjects(data)); },
+     []);
