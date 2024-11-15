5ce183a38ed4dce45c996a192b04af9fdfdf1fd5
Perpetual Akinyi <you@example.com>
2024-11-15 03:02:58 +0300

    updated skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Skills() {
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/skills')
+      .then(response => response.json())
+      .then(data => setSkills(data));
+  }, []);
+
   return (
-    <div>Skills</div>
-  )
+    <div>
+      <h1>My Skills</h1>
+      <ul>
+        {skills.map(skill => (
+          <li key={skill.id}>
+            {skill.name} - {skill.level}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Skills
+export default Skills;
