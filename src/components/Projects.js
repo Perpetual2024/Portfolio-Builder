ca078b18cdc87318908db26646cff6985b0701fc
Perpetual Akinyi <you@example.com>
2024-11-15 03:10:39 +0300

    finished setting up my project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,3 +4,14 @@ function Projects() { const [projects, setProjects] = useState([]);
     .then(response => response.json()) 
     .then(data => setProjects(data)); },
      []);
+     return ( <div> <h1>My Projects</h1> 
+     <ul> {projects.map(project => ( <li key={project.id}>
+       <h2>{project.title}</h2>
+        <img src={project.image} alt={project.title} />
+         <p>{project.description}</p> 
+         <a href={project.url}>View Project</a> 
+         </li> 
+        ))} 
+         </ul>
+          </div> ); }
+          export default Projects;
